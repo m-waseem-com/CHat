55b3ea958d0df946b2b45fc07864e9de57a4db07
Muhammad Waseem <105275712+m-waseem-com@users.noreply.github.com>
2022-09-21 20:24:10 +0500

    Uplaod full project

---
+++ WebApplication1/Models/msg.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class msg
+    {
+        public int id { get; set; }
+        public string Msg { get; set; }
+
+    }
+}
---
+++ WebApplication1/Scripts/newone.js
@@ -0,0 +1,13 @@
+﻿$(document).on('click', '#btnAddComments1', function () {
+    var editorData = CKEDITOR.instances['Msg'].getData();
+    var url1 = window.location.href; debugger
+    $.ajax({
+        url: "/Home/AddComent",
+        type: 'POST',
+        data: {
+            msg : editorData,
+            path : url1
+        }
+
+    });
+})
